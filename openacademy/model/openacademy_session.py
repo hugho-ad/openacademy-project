1439c5b275e857663408613191c9a6985404893d
Hugo Adan <hugo@vauxoo.com>
2015-01-08 10:35:35 -0600

    [REF]openacademy: Adding active field and a defautl for fields date

--- openacademy/model/openacademy_session.py
+++ openacademy/model/openacademy_session.py
@@ -5,7 +5,7 @@ class Session (models.Model):
     _name = 'openacademy.session'
 
     name = fields.Char(required=True)
-    start_date = fields.Date()
+    start_date = fields.Date(default=fields.Date.today)
     duration = fields.Float(digits=(6, 2), help="Duration in days")
     seats = fields.Integer(string="Number of seats")
     instructor_id = fields.Many2one('res.partner', string="Instructor",
@@ -17,6 +17,7 @@ class Session (models.Model):
         ondelete='cascade', string="Course", required=True)
     attendee_ids = fields.Many2many('res.partner', string="Attendees")
     taken_seats = fields.Float(string="Taken seats",compute='_taken_seats')
+    active = fields.Boolean(default=True)
 
     @api.one
     @api.depends('seats', 'attendee_ids')
@@ -25,3 +26,5 @@ class Session (models.Model):
             self.taken_seats = 0.0
         else:
             self.taken_seats = 100.0 * len(self.attendee_ids) / self.seats
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
