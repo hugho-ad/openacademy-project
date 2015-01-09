1db1fe26f51c1a9f32d2ce77ca9ae11d96043bc2
Hugo Adan <hugo@vauxoo.com>
2015-01-09 12:12:07 -0600

    [Ref]openacademy : Adding Calendar view on session Model

--- openacademy/model/openacademy_session.py
+++ openacademy/model/openacademy_session.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from datetime import timedelta
 from openerp import api, exceptions, fields, models
 
 class Session (models.Model):
@@ -17,7 +18,10 @@ class Session (models.Model):
         ondelete='cascade', string="Course", required=True)
     attendee_ids = fields.Many2many('res.partner', string="Attendees")
     taken_seats = fields.Float(string="Taken seats",compute='_taken_seats')
+    end_date = fields.Date(string="End Date", store=True,
+        compute='_get_end_date', inverse='_set_end_date')
     active = fields.Boolean(default=True)
+
 
     @api.one
     @api.depends('seats', 'attendee_ids')
@@ -50,6 +54,30 @@ class Session (models.Model):
         if self.instructor_id and self.instructor_id in self.attendee_ids:
             raise exceptions.ValidationError("A session's instructor can't be an attendee")
 
+    @api.one
+    @api.depends('start_date','duration')
+    def _get_end_date(self):
+        if not(self.start_date and self.duration):
+            self.end_date=self.start_date
+            return
+
+        # Add duration to start_date, but: Monday + 5 days = Saturday, so
+        # subtract one second to get on Friday instead
+        start = fields.Datetime.from_string(self.start_date)
+        duration = timedelta(days=self.duration, seconds=-1)
+        self.end_date = start + duration
+
+
+    @api.one
+    def _set_end_date(self):
+        if not (self.start_date and self.end_date):
+            return
+
+        # Compute the difference between dates, but: Friday - Monday = 4 days,
+        # so add one day to get 5 days instead
+        start_date = fields.Datetime.from_string(self.start_date)
+        end_date = fields.Datetime.from_string(self.end_date)
+        self.duration = (end_date - start_date).days + 1
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
 
