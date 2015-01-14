66b169fc31aa459f979a6cf2f96dd9fcd65dfc04
Hugo Adan <hugo@vauxoo.com>
2015-01-14 13:10:02 -0600

    [REF]openacademy: refactorig wizard to show sessions instead of sesion (many2many field)

--- openacademy/wizard/openacademy_wizard.py
+++ openacademy/wizard/openacademy_wizard.py
@@ -6,13 +6,14 @@ class Wizard(models.TransientModel):
     _name = 'openacademy.wizard'
 
     def _default_session(self):
-        return self.env['openacademy.session'].browse(self._context.get('active_id'))
+        return self.env['openacademy.session'].browse(self._context.get('active_ids'))
 
-    session_id = fields.Many2one('openacademy.session',
-        string="Session", required=True, default=_default_session)
+    session_ids = fields.Many2many('openacademy.session',
+        string="Sessions", required=True, default=_default_session)
     attendee_ids = fields.Many2many('res.partner', string="Attendees")
 
     @api.multi
     def subscribe(self):
-        self.session_id.attendee_ids |= self.attendee_ids
-        return {}
+        for session in self.session_ids:
+            session.attendee_ids |= self.attendee_ids
+            return {}
